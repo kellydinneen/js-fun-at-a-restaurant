e4c3d84ec4671c77340fb39fc706f067b5e94343
kellydinneen <kgdinneen@gmail.com>
2020-10-08 17:24:26 -0600

    Complete iteration 1

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,32 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {name, price, type};
+}
+
+function addIngredients(ingredient, recipe) {
+  recipe.includes(ingredient) ? recipe = recipe : recipe.push(ingredient);
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(initialPrice) {
+  return 0.9 * initialPrice;
+}
+
+function createRecipe(title, ingredients, type) {
+  return {title, ingredients, type};
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
